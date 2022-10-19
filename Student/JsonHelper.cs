7d9777be781bab4d2693dbee6b07fb8cb5fceafa
JusteNav <44645701+JusteNav@users.noreply.github.com>
2022-10-19 17:08:46 +0300

    Added JSON file read/write in a separate project.

---
+++ Student/JsonHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Student
+{
+    internal class JsonHelper
+    {
+
+        private readonly static string _filePathStudent = @"../../../../Student/Student.json";
+
+        public static Student LoadStudent()
+        {
+            var jsonString = File.ReadAllText(_filePathStudent);
+            var student = JsonSerializer.Deserialize<Student>(jsonString);
+            return student;
+        }
+
+        public static void SaveStudent(Student student)
+        {
+            string jsonString = JsonSerializer.Serialize(student);
+            File.WriteAllText(_filePathStudent, jsonString);
+        }
+    }
+}
---
+++ Student/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             var student = JsonHelper.LoadStudent();
+
+            Console.WriteLine($"Student name: {student.Name}");
+
+            var newStudent = new Student
+            {
+                Id = 101,
+                Name = "Bilbo",
+                Surname = "Baggins",
+                Grades = new int[] { 8, 5, 9, 9, 7, 10 }
+            };
+
+            JsonHelper.SaveStudent(newStudent);
+            var loadedNewStudent = JsonHelper.LoadStudent();
+
+            Console.WriteLine($"New student name: {loadedNewStudent.Name}");
+        }
+    }
+}
